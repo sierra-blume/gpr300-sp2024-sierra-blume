8e40c6987f5ff3b9ee49a0553e2989c1bb02bd51
Sierra Blume <sierra.blume@mymail.champlain.edu>
2025-01-24 09:47:41 -0500

    Made it to page 7 of the instructions

--- assignments/assignment0/main.cpp
+++ assignments/assignment0/main.cpp
@@ -7,6 +7,9 @@
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/camera.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 GLFWwindow* initWindow(const char* title, int width, int height);
@@ -22,6 +25,23 @@ int main() {
 	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+	//Making a shader with the shader files in the assets folder
+	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	//Loading a 3D model for us to render
+	ew::Model monkeyModel = ew::Model("assets/suzanne.obj");
+
+	//Creating a camera for us to view our model
+	ew::Camera camera;
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f);  //Look at the center of the scene
+	camera.aspectRatio = (float)screenWidth / screenHeight;  //Should be updated every frame or in framebufferSizeCallback to keep the aspect ratio correct after resizing the window
+	camera.fov = 60.0f;  //Vertical field of view, in degrees
+
+	//Setting some global OpenGL variables
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);  //Back face culling
+	glEnable(GL_DEPTH_TEST);  //Depth testing
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
@@ -31,7 +51,14 @@ int main() {
 
 		//RENDER
 		glClearColor(0.6f,0.8f,0.92f,1.0f);
+		//Clears backbuffer color and depth values
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//Set shader uniforms and draw
+		shader.use();
+		shader.setMat4("_Model", glm::mat4(1.0f));
+		shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
+		monkeyModel.draw();  //Draws monkey model using current shader
 
 		drawUI();
 
