45df5f2e6f6b65a2ca645201017ffef682349c58
Sierra Blume <sierra.blume@mymail.champlain.edu>
2025-03-04 01:40:58 -0500

    Added the basics for the Vec3Key, AnimationClip and Animator, but the files are not communicating with each other for some reason at the moment. Will try again tomorrow

--- assignments/assignment4/assets/AnimationClip.h
+++ assignments/assignment4/assets/AnimationClip.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "Vec3Key.h"
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <vector>
+#include <algorithm>
+
+class AnimationClip
+{
+	float duration;
+	std::vector<Vec3Key> positionKeys;
+	std::vector<Vec3Key> rotationKeys;
+	std::vector<Vec3Key> scaleKeys;
+};
--- assignments/assignment4/assets/Vec3Key.h
+++ assignments/assignment4/assets/Vec3Key.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <glm/glm.hpp>
+#include <vector>
+
+class Vec3Key {
+public:
+	float mTime;
+	glm::vec3 mValue;
+
+	Vec3Key(float time, glm::vec3 value)
+	{
+		this->mTime = time;
+		this->mValue = value;
+	}
+	Vec3Key(float time)
+	{
+		this->mTime = time;
+		mValue = glm::vec3(0);
+	}
+	Vec3Key()
+	{
+		mTime = 0;
+		mValue = glm::vec3(0);
+	}
+};
